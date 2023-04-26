a6f2feebc7594924af831a9da87fa8212fc3472e
unyss98 <eunicedunyo@gmail.com>
2023-04-26 16:29:45 -0700

    initial commit for task 1

--- printf.c
+++ printf.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 /**
  *  _printf - produces output according to a format
@@ -14,9 +15,8 @@
 int _printf(const char *format, ...)
 {
 	va_list list;
-	int i, j, cv, count = 0;
+	int i, j, k, cv, dv, temp, dd, nd, count = 0;
 	char *sv;
-	char n[4] = {'n', 'u', 'l', 'l'};
 
 	va_start(list, format);
 	if (format == NULL || strcmp(format, "%") == 0)
@@ -33,14 +33,25 @@ int _printf(const char *format, ...)
 				case 's':
 					sv = va_arg(list, char*);
 					if (sv == NULL)
-						_printf("%s", n[4]), count += 4;
+					write(STDOUT_FILENO, "null", 4), count += 4;
 					for (j = 0; sv[j] != '\0'; j++)
 						putchar(sv[j]), count++;
+					break;
+				case 'd':
+				case 'i':
+					dv = va_arg(list, int);
+					nd = 0, temp = dv;
+					while (temp != 0)
+						nd++, temp /= 10;
+					if (dv < 0)
+						putchar('-'), dv = -dv;
+					for (k = nd - 1; k >= 0; k--)
+					       dd = dv / (int) pow(10, k) % 10, putchar('0' + dd);
 					break;
 				case '%':
 					putchar('%'), count++;
 					break;
-				default:
+					default:
 					putchar('%'), putchar(format[i]), count += 2;
 					break;
 			}
